7493d710d040cf3ee068c51bcbbd5bfbf3efac31
IriemDaLuz <yenduuca2005@gmail.com>
2024-11-07 18:12:15 +0000

    Reubicamos las imagenes  a /resource

--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -1,23 +1,23 @@
 
 data class Account(val name : String, val image: String)
 val accounts = listOf<Account>(
-    Account("Unay Porez","/img/accounts/img2.jpeg"),
-    Account("Fornondo Mentelongo","/img/accounts/img1.jpeg"),
-    Account("Pabio Raton","/img/accounts/img3.jpeg")
+    Account("Unay Porez","img/accounts/img2.jpeg"),
+    Account("Fornondo Mentelongo","img/accounts/img1.jpeg"),
+    Account("Pabio Raton","img/accounts/img3.jpeg")
 )
 
 data class sugPublicaciones(val image: String)
 val sugPublicacion = listOf<sugPublicaciones>(
-    sugPublicaciones("/img/publicaciones/publicacion1.jpeg"),
-    sugPublicaciones("/img/publicaciones/publicacion2.jpeg"),
-    sugPublicaciones("/img/publicaciones/publicacion3.jpeg"),
-    sugPublicaciones("/img/publicaciones/publicacion4.jpeg"),
-    sugPublicaciones("/img/publicaciones/publicacion5.jpeg"),
-    sugPublicaciones("/img/publicaciones/publicacion6.jpeg")
+    sugPublicaciones("img/publicaciones/publicacion1.jpeg"),
+    sugPublicaciones("img/publicaciones/publicacion2.jpeg"),
+    sugPublicaciones("img/publicaciones/publicacion3.jpeg"),
+    sugPublicaciones("img/publicaciones/publicacion4.jpeg"),
+    sugPublicaciones("img/publicaciones/publicacion5.jpeg"),
+    sugPublicaciones("img/publicaciones/publicacion6.jpeg")
 )
 
 data class Publicaciones(val name: String,val image: String,val publicacion: String,val descripcion :String)
 val publicaciones = listOf<Publicaciones>(
-    Publicaciones("Unay Porez","/img/accounts/img2.jpeg","/img/publicaciones/publicacion2.jpeg","In the Dark"),
-    Publicaciones("Fornondo Mentelongo","/img/accounts/img1.jpeg","/img/publicaciones/publicacion1.jpeg","Sun & Beach"),
+    Publicaciones("Unay Porez","img/accounts/img2.jpeg","img/publicaciones/publicacion2.jpeg","In the Dark"),
+    Publicaciones("Fornondo Mentelongo","img/accounts/img1.jpeg","img/publicaciones/publicacion1.jpeg","Sun & Beach"),
    )
