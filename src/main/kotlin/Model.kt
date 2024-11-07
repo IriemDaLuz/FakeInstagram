294e2f72cec119afb10f040a995e3591910b8331
IriemDaLuz <yenduuca2005@gmail.com>
2024-11-07 17:56:01 +0000

    Model: Renombramos las rutas de las imagenes

--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -1,23 +1,23 @@
 
 data class Account(val name : String, val image: String)
 val accounts = listOf<Account>(
-    Account("Unay Porez","img1.jpeg"),
-    Account("Fornondo Mentelongo","img2.jpeg"),
-    Account("Pabio Raton","img3.jpeg")
+    Account("Unay Porez","img.accounts.img2.jpeg"),
+    Account("Fornondo Mentelongo","img.accounts.img1.jpeg"),
+    Account("Pabio Raton","img.accounts.img3.jpeg")
 )
 
 data class sugPublicaciones(val image: String)
 val sugPublicacion = listOf<sugPublicaciones>(
-    sugPublicaciones("publicacion1.jpeg"),
-    sugPublicaciones("publicacion2.jpeg"),
-    sugPublicaciones("publicacion3.jpeg"),
-    sugPublicaciones("publicacion4.jpeg"),
-    sugPublicaciones("publicacion5.jpeg"),
-    sugPublicaciones("publicacion6.jpeg")
+    sugPublicaciones("img.publicaciones.publicacion1.jpeg"),
+    sugPublicaciones("img.publicaciones.publicacion2.jpeg"),
+    sugPublicaciones("img.publicaciones.publicacion3.jpeg"),
+    sugPublicaciones("img.publicaciones.publicacion4.jpeg"),
+    sugPublicaciones("img.publicaciones.publicacion5.jpeg"),
+    sugPublicaciones("img.publicaciones.publicacion6.jpeg")
 )
 
 data class Publicaciones(val name: String,val image: String,val publicacion: String,val descripcion :String)
 val publicaciones = listOf<Publicaciones>(
-    Publicaciones("Unay Porez","img1.jpeg","publicacion1.jpeg","Sun & Beach"),
-    Publicaciones("Fornondo Mentelongo","img2.jpeg","publicacion2.jpeg","In the Dark"),
+    Publicaciones("Unay Porez","img2.jpeg","img.publicaciones.publicacion2.jpeg","In the Dark"),
+    Publicaciones("Fornondo Mentelongo","img1.jpeg","img.publicaciones.publicacion1.jpeg","Sun & Beach"),
    )
