d828166fb1166628d7019433655f1e4ea59ed04c
IriemDaLuz <yenduuca2005@gmail.com>
2024-11-07 19:41:01 +0000

    Añadimos mas cuentas y mas fotos

--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -3,7 +3,9 @@ data class Account(val name : String, val image: String)
 val accounts = listOf<Account>(
     Account("Unay Porez","img/accounts/img2.jpeg"),
     Account("Fornondo Mentelongo","img/accounts/img1.jpeg"),
-    Account("Pabio Raton","img/accounts/img3.jpeg")
+    Account("Pabio Raton","img/accounts/img3.jpeg"),
+    Account("Chimon Otaku","img/accounts/img4.jpeg"),
+    Account("Esclavo Nigga","img/accounts/img5.jpeg")
 )
 
 data class sugPublicaciones(val image: String)
