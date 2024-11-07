9d07f4faa283ec6b00d5ec607e43ccd58f689083
Iriemst <iriemdaluz22@gmail.com>
2024-11-07 13:54:21 +0000

    Model: Modificamos Publicaciones solo tenemos dos y creamos Sug-publicaciones que solo son fotos

--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -1,21 +1,23 @@
 
 data class Account(val name : String, val image: String)
-
 val accounts = listOf<Account>(
     Account("Unay Porez","img1.jpeg"),
     Account("Fornondo Mentelongo","img2.jpeg"),
     Account("Pabio Raton","img3.jpeg")
 )
 
+data class sugPublicaciones(val image: String)
+val sugPublicaciones = listOf<sugPublicaciones>(
+    sugPublicaciones("publicacion1.jpeg"),
+    sugPublicaciones("publicacion2.jpeg"),
+    sugPublicaciones("publicacion3.jpeg"),
+    sugPublicaciones("publicacion4.jpeg"),
+    sugPublicaciones("publicacion5.jpeg"),
+    sugPublicaciones("publicacion6.jpeg")
+)
+
 data class Publicaciones(val name: String,val image: String,val publicacion: String,val descripcion :String)
-
-
 val publicaciones = listOf<Publicaciones>(
     Publicaciones("Unay Porez","img1.jpeg","publicacion1.jpeg","Sun & Beach"),
     Publicaciones("Fornondo Mentelongo","img2.jpeg","publicacion2.jpeg","In the Dark"),
-    Publicaciones("Pabio Raton","img3.jpeg","publicacion3.jpeg","..."),
-    Publicaciones("Fornondo Mentelongo","img2.jpeg","publicacion2.jpeg","..."),
-    Publicaciones("Pabio Raton","img3.jpeg","publicacion3.jpeg","..."),
-    Publicaciones("Fornondo Mentelongo","img2.jpeg","publicacion2.jpeg","...")
-
-)
+   )
