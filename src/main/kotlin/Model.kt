697e8f7f0d0d06d9544fe56a1b06dfc9fb364e4c
Iriemst <iriemdaluz22@gmail.com>
2024-11-07 13:32:49 +0000

    Model: Creamos la calse de datos Account

---
+++ src/main/kotlin/Model.kt
@@ -0,0 +1,8 @@
+
+data class Account(val name : String, val image: String)
+
+val accounts = listOf<Account>(
+    Account("Unay Porez","img1.jpeg"),
+    Account("Fornondo Mentelongo","img2.jpeg"),
+    Account("Pabio Raton","img3.jpeg")
+)
